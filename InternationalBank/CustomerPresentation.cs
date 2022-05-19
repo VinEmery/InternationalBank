159ae7001fe63d57ab848e81d2ff1e35a2ea4b44
vinemery <emeryvin@gmail.com>
2022-05-19 11:07:10 +0200

    Inital Commit

---
+++ InternationalBank/CustomerPresentation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using InternationalBank.entities;
+using InternationalBank.exception;
+using InternationalBank.BusinessLayer;
+
+namespace InternationalBank
+{
+    static public class CustomerPresentation
+    {
+        internal static void AddCustomer()
+        {
+
+        }
+    }
+}
